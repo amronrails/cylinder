fc9163d9152e2b0f750c1caf6fa2d791be7a8179
amronrails <amre.abdelhady@gmail.com>
2016-01-17 06:51:02 +0200

    cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '46.101.195.240', user: 'deploy', roles: %w{web app db}
+server '46.101.144.77', user: 'deploy', roles: %w{web app db}
 
 # server-based syntax
 # ======================
