a87f2cbbccf16552b1b90f551535e6773b2535eb
amronrails <amre.abdelhady@gmail.com>
2016-02-10 04:13:46 +0200

    6-cylinder.com

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '46.101.144.77', user: 'deploy', roles: %w{web app db}
+server '6-cylinder.com', user: 'deploy', roles: %w{web app db}
 
 # server-based syntax
 # ======================
