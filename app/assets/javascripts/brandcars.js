70b5a62bc24e316a05600d0759f863e27eba7c5d
amronrails <amre.abdelhady@gmail.com>
2016-07-01 10:57:43 +0200

    cb

---
+++ app/assets/javascripts/brandcars.js
@@ -0,0 +1,28 @@
+$(document).ready(function( ) {
+    $(".tfasel").hide();
+    $(".eh").hide();
+    var s=$(".carbrands").offset();
+//  alert(s.left);
+
+    var s=$(".carbrands").width();
+    $(".tfasel").css("width",s);
+});
+
+$(".info").click(function(){
+    $(".tfasel").not($(this).find(".tfasel")).hide(500)
+    $(".eh").not($(this).find(".eh")).hide(500)
+
+    $(this).find(".tfasel").slideToggle(500);
+    $(this).find(".eh").slideToggle(500);
+
+});
+
+$(window).scroll(function() {
+    if( $(this).scrollTop() > 130 ) {
+        $(".main-nav").addClass("main-nav-scrolled");
+        $(".main").addClass("mainscrolled");
+    } else {
+        $(".main-nav").removeClass("main-nav-scrolled");
+        $(".main").removeClass("mainscrolled");
+    }
+});
